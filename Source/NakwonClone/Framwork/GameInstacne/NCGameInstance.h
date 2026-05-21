ce5f0930bbaf86f312c79c3a83fb90b826e12962
SEOWOOJEONG <unha1@naver.com>
2026-05-21 12:15:10 +0900

    [서우정 add] framework

---
+++ Source/NakwonClone/Framwork/GameInstacne/NCGameInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NCGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAKWONCLONE_API UNCGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
---
+++ Source/NakwonClone/Framwork/PlayerState/NCPlayerState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "NCPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAKWONCLONE_API ANCPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+};
