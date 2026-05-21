e49909e08b534d44d2994faff23c8869a6842c95
tkd6966 <tkd6966@gmail.com>
2026-05-21 17:17:57 +0900

    [하상빈 Add] Enhanced Input 기반 캐릭터 이동 및 시점 회전 연산 로직 구현

--- Source/NakwonClone/Player/PlayerCharacter/NCPlayerCharacter.cpp
+++ Source/NakwonClone/Player/PlayerCharacter/NCPlayerCharacter.cpp
@@ -52,8 +52,30 @@ void ANCPlayerCharacter::SetupPlayerInputComponent(class UInputComponent* Player
 
 void ANCPlayerCharacter::Move(const FInputActionValue& Value)
 {
+	FVector2D MovementVector = Value.Get<FVector2D>();
+	
+	if (Controller != nullptr)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+        
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
 }
 
 void ANCPlayerCharacter::Look(const FInputActionValue& Value)
 {
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+	
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+        
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
