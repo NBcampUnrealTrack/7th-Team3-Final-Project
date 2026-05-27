64ca8d5d8de8e686806beb5ba6309ad7850ac226
shy3939 <kpky33@naver.com>
2026-05-27 20:42:36 +0900

    [유시환 add] 워커 BT 상태 전환 + 액터 회전
    - 좀비 메시 대신 큐브 메시를 사용함
    - 상태 전환은 애니메이션을 통한 출력이 아닌, 큐브 메시의 머테리얼 전환을 통해 확인 가능

---
+++ Source/NakwonClone/Zombie/AI/BTNode/BTTask_Attack.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NakwonClone/Zombie/AI/BTNode/BTTask_Attack.h"
+#include "AIController.h"
+#include "NakwonClone/Zombie/ZombieCharacter/Base/VGMonsterCharacterBase.h"
+#include "NakwonClone/Zombie/ZombieCharacter/Walker/VGMonsterWalker.h"
+
+UBTTask_Attack::UBTTask_Attack()
+{
+	NodeName = "Attack";
+}
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AVGMonsterCharacterBase* Monster = Cast<AVGMonsterCharacterBase>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!Monster) return EBTNodeResult::Failed;
+	
+	AVGMonsterWalker* Walker = Cast<AVGMonsterWalker>(OwnerComp.GetAIOwner()->GetPawn());
+	if (Walker) Walker->SetMonsterState(EMonsterState::Attack);
+	
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/NakwonClone/Zombie/AI/BTNode/BTTask_Dead.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NakwonClone/Zombie/AI/BTNode/BTTask_Dead.h"
+#include "AIController.h"
+#include "NakwonClone/Zombie/ZombieCharacter/Base/VGMonsterCharacterBase.h"
+#include "NakwonClone/Zombie/ZombieCharacter/Walker/VGMonsterWalker.h"
+
+UBTTask_Dead::UBTTask_Dead()
+{
+	NodeName = "Dead";
+}
+
+EBTNodeResult::Type UBTTask_Dead::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AVGMonsterCharacterBase* Monster = Cast<AVGMonsterCharacterBase>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!Monster) return EBTNodeResult::Failed;
+
+	Monster->Dead();
+
+	// BT 중단
+	OwnerComp.StopTree(EBTStopMode::Safe);
+
+	AVGMonsterWalker* Walker = Cast<AVGMonsterWalker>(OwnerComp.GetAIOwner()->GetPawn());
+	if (Walker) Walker->SetMonsterState(EMonsterState::Dead);
+	
+	return EBTNodeResult::Succeeded;
+}
