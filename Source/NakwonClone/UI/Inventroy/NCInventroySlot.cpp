7107181ecf9ea60dab2296b60c24308b46d66f09
wksjdb87-design <wksjdb87@gmail.com>
2026-05-29 19:14:02 +0900

    [강민승 add] 인벤토리 UI 연결

--- Source/NakwonClone/UI/Inventroy/NCInventroySlot.cpp
+++ Source/NakwonClone/UI/Inventroy/NCInventroySlot.cpp
@@ -1,11 +1,24 @@
 ﻿#include "NCInventroySlot.h"
 
+#include "GameplayTagContainer.h"
+
 void UNCInventroySlot::NativeConstruct()
 {
 	Super::NativeConstruct();
 }
 
-void UNCInventroySlot::UpdateItemSlot(UTexture2D* ItemIcon)
+void UNCInventroySlot::SetSlotData(int32 InIndex, FGameplayTag InTag, int32 InQuantity)
 {
+	SlotIndex = InIndex;
 	
+	if (InTag.IsValid())
+	{
+		ItemCountText->SetText(FText::AsNumber(InQuantity));
+		ItemImage->SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		ItemCountText->SetText(FText::GetEmpty());
+		ItemImage->SetVisibility(ESlateVisibility::Hidden);
+	}
 }
