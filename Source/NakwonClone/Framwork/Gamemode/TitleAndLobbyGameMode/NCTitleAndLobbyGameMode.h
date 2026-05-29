ea6b35b5fa9987fe679079a3f3f63fef7a896d49
SEOWOOJEONG <unha1@naver.com>
2026-05-29 17:17:16 +0900

    [서우정 add] 타이틀로비 레벨 및 게임모드 생성 05-29

---
+++ Source/NakwonClone/Framwork/Gamemode/TitleAndLobbyGameMode/NCTitleAndLobbyGameMode.h
@@ -0,0 +1,18 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NCTitleAndLobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAKWONCLONE_API ANCTitleAndLobbyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	ANCTitleAndLobbyGameMode();
+};
