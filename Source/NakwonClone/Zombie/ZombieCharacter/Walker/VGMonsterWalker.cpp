831ae64991d272d15538d9c4cc9bb913f5f26784
shy3939 <kpky33@naver.com>
2026-06-02 19:17:47 +0900

    [유시환] 좀비 애니메이션 적용

--- Source/NakwonClone/Zombie/ZombieCharacter/Walker/VGMonsterWalker.cpp
+++ Source/NakwonClone/Zombie/ZombieCharacter/Walker/VGMonsterWalker.cpp
@@ -26,6 +26,14 @@ void AVGMonsterWalker::BeginPlay()
 		GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
 	}
 	
+	if (MonsterAttributeSet)
+	{
+		// 피격 이벤트 오면 HandleHit 실행
+		MonsterAttributeSet->OnHitReceived.AddDynamic(this, &AVGMonsterWalker::HandleHit);
+		// 사망 이벤트 오면 HandleDead 실행
+		MonsterAttributeSet->OnDead.AddDynamic(this, &AVGMonsterWalker::HandleDead);
+	}
+	
 	// 시작 시 순찰 상태로 초기화
 	SetMonsterState(EMonsterState::Move);
 }
@@ -60,4 +68,34 @@ void AVGMonsterWalker::SetMonsterState(EMonsterState NewState)
 	{
 		PlayAnimMontage(TargetMontage);
 	}
+}
+
+void AVGMonsterWalker::HandleHit()
+{
+	if (MonsterAttributeSet->GetHealth() <= 0.f) return;
+	SetMonsterState(EMonsterState::Hit);
+}
+
+void AVGMonsterWalker::HandleDead()
+{
+	if (AAIController* AIC = Cast<AAIController>(GetController()))
+	{
+		AIC->StopMovement();
+		AIC->UnPossess();
+	}
+
+	SetActorEnableCollision(false);
+
+	float Duration = PlayAnimMontage(AnimDead);
+
+	FTimerHandle FreezeHandle;
+	GetWorldTimerManager().SetTimer(FreezeHandle, [this]()
+	{
+		if (UAnimInstance* AnimInst = GetMesh()->GetAnimInstance())
+		{
+			AnimInst->Montage_Pause(AnimDead);
+		}
+	}, 1.16f, false);
+
+	SetLifeSpan(200.f);
 }
