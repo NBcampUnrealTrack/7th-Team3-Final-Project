f47ac77c3e0f9708da1d7916ea07538e00891abd
shy3939 <kpky33@naver.com>
2026-06-08 09:30:49 +0900

    [유시환] 워커 상태 전환 오류 해결

---
+++ Source/NakwonClone/Zombie/AI/BTNode/BTTask_Chase.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_Chase.h"
+#include "AIController.h"
+#include "NakwonClone/Zombie/ZombieCharacter/Walker/VGMonsterWalker.h"
+
+UBTTask_Chase::UBTTask_Chase()
+{
+	NodeName = "Chase";
+}
+
+EBTNodeResult::Type UBTTask_Chase::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AVGMonsterWalker* Walker = Cast<AVGMonsterWalker>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!Walker) return EBTNodeResult::Failed;
+	
+	Walker->SetMonsterState(EMonsterState::Chase);
+	
+	return EBTNodeResult::Succeeded;
+}
