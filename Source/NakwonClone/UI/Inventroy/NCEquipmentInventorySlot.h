62854edae0c847d7d8a298bb0b30ab77e0f804d3
wksjdb87-design <wksjdb87@gmail.com>
2026-06-09 19:06:16 +0900

    [강민승 fix] 장비 슬롯 연동 수정

--- Source/NakwonClone/UI/Inventroy/NCEquipmentInventorySlot.h
+++ Source/NakwonClone/UI/Inventroy/NCEquipmentInventorySlot.h
@@ -29,7 +29,7 @@ public:
 	UPROPERTY(meta = (BindWidget))
 	UImage* EquipmentItemImage;
 	
-	UPROPERTY(BlueprintReadWrite)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 SlotIndex;
 	
 	UPROPERTY(BlueprintReadOnly)
