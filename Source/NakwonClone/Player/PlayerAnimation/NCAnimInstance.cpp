28c3bf9b3bbde0ff1462b38b4798a5e2cb2e076f
0Kaede0 <hun151800@gmail.com>
2026-06-10 19:05:46 +0900

    [add성헌호]무기타입 감지

--- Source/NakwonClone/Player/PlayerAnimation/NCAnimInstance.cpp
+++ Source/NakwonClone/Player/PlayerAnimation/NCAnimInstance.cpp
@@ -4,6 +4,9 @@
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "KismetAnimationLibrary.h"
+#include "NakwonClone/Player/PlayerCharacter/NCPlayerCharacter.h"
+#include "NakwonClone/Player/PlayerAnimation/NCCombatComponent.h"
+#include "NakwonClone/Common/NCGameplayTags.h"
 
 void UNCAnimInstance::NativeInitializeAnimation()
 {
@@ -13,6 +16,12 @@ void UNCAnimInstance::NativeInitializeAnimation()
     if (OwnerCharacter)
     {
         MovementComponent = OwnerCharacter->GetCharacterMovement();
+
+        // CombatComponent 캐싱
+        if (ANCPlayerCharacter* PlayerCharacter = Cast<ANCPlayerCharacter>(OwnerCharacter))
+        {
+            CachedCombatComponent = PlayerCharacter->GetCombatComponent();
+        }
     }
 }
 
@@ -37,4 +46,13 @@ void UNCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
     bIsInAir = MovementComponent->IsFalling();
     bIsCrouching = MovementComponent->IsCrouching();
     bShouldMove = Speed > 3.f && !MovementComponent->GetCurrentAcceleration().IsNearlyZero();
+
+    // 무기 타입 업데이트
+    if (CachedCombatComponent)
+    {
+        CurrentWeaponTypeTag = CachedCombatComponent->GetEquippedWeaponTypeTag();
+        bHasWeapon = !CurrentWeaponTypeTag.MatchesTagExact(NCWeapon::Type_Unarmed);
+        bIsOneHandedWeapon = CurrentWeaponTypeTag.MatchesTagExact(NCWeapon::Type_OneHanded);
+        bIsTwoHandedWeapon = CurrentWeaponTypeTag.MatchesTagExact(NCWeapon::Type_TwoHanded);
+    }
 }
--- Source/NakwonClone/Player/PlayerAnimation/NCAnimInstance.h
+++ Source/NakwonClone/Player/PlayerAnimation/NCAnimInstance.h
@@ -4,10 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "GameplayTagContainer.h"
 #include "NCAnimInstance.generated.h"
 
 class ACharacter;
 class UCharacterMovementComponent;
+class ANCPlayerCharacter;
+class UNCCombatComponent;
 
 UCLASS()
 class NAKWONCLONE_API UNCAnimInstance : public UAnimInstance
@@ -25,6 +28,10 @@ protected:
     UPROPERTY(BlueprintReadOnly, Category = "References")
     TObjectPtr<UCharacterMovementComponent> MovementComponent;
 
+    // 캐싱 (매 프레임 Find 방지)
+    UPROPERTY()
+    TObjectPtr<UNCCombatComponent> CachedCombatComponent;
+
     UPROPERTY(BlueprintReadOnly, Category = "Locomotion")
     float Speed = 0.f;
 
@@ -39,4 +46,16 @@ protected:
 
     UPROPERTY(BlueprintReadOnly, Category = "Locomotion")
     bool bIsCrouching = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
+    FGameplayTag CurrentWeaponTypeTag;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
+    bool bHasWeapon = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
+    bool bIsOneHandedWeapon = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
+    bool bIsTwoHandedWeapon = false;
 };
