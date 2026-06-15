92d3bdffd55f6c6129aa7f5a3611bf94463569db
0Kaede0 <hun151800@gmail.com>
2026-06-15 15:27:44 +0900

    [add] 양손 ik추가

--- Source/NakwonClone/Player/PlayerAnimation/NCAnimInstance.cpp
+++ Source/NakwonClone/Player/PlayerAnimation/NCAnimInstance.cpp
@@ -3,6 +3,7 @@
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "KismetAnimationLibrary.h"
+#include "AbilitySystemComponent.h"
 
 #include "NakwonClone/Player/PlayerCharacter/NCPlayerCharacter.h"
 #include "NakwonClone/Player/PlayerAnimation/NCCombatComponent.h"
@@ -66,6 +67,15 @@ void UNCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
     bShouldMove = Speed > 3.f && !MovementComponent->GetCurrentAcceleration().IsNearlyZero();
 
     UpdateWeaponAndBlendSpace();
+
+    // 헌호수정 - 전용 서버에서는 IK 계산 불필요
+    if (UWorld* World = GetWorld())
+    {
+        if (!World->IsNetMode(NM_DedicatedServer))
+        {
+            UpdateLeftHandIK();
+        }
+    }
 }
 
 void UNCAnimInstance::UpdateWeaponAndBlendSpace()
@@ -118,5 +128,77 @@ void UNCAnimInstance::UpdateWeaponAndBlendSpace()
         CurrentLocomotionBlendSpace = bIsCrouching
             ? DefaultUnarmedBS_Crouching.LoadSynchronous()
             : DefaultUnarmedBS_Standing.LoadSynchronous();
+    }
+}
+
+// 헌호수정 - 양손 IK 위치 업데이트 (클라이언트 전용)
+void UNCAnimInstance::UpdateLeftHandIK()
+{
+    UNCCombatComponent* ActiveCombatComponent = CachedCombatComponent
+        ? CachedCombatComponent.Get()
+        : CombatComponent.Get();
+
+    // 공격 중에는 IK 끔 (애니메이션 자체에 맡김)
+    if (OwnerCharacter)
+    {
+        UAbilitySystemComponent* ASC = OwnerCharacter->FindComponentByClass<UAbilitySystemComponent>();
+        bIsAttacking = ASC ? ASC->HasMatchingGameplayTag(NCWeapon::Action_Attacking) : false;
+    }
+
+    if (!ActiveCombatComponent || !ActiveCombatComponent->IsWeaponEquipped() || bIsAttacking)
+    {
+        bUseLeftHandIK = false;
+        bUseRightHandIK = false;
+        return;
+    }
+
+    const FNCWeaponData* WeaponData = ActiveCombatComponent->GetEquippedWeaponData();
+    if (!WeaponData || !WeaponData->bUseTwoHandIK)
+    {
+        bUseLeftHandIK = false;
+        bUseRightHandIK = false;
+        return;
+    }
+
+    AActor* WeaponActor = ActiveCombatComponent->GetSpawnedWeaponActor();
+    if (!WeaponActor)
+    {
+        bUseLeftHandIK = false;
+        bUseRightHandIK = false;
+        return;
+    }
+
+    // 스켈레탈 메시 먼저 시도, 없으면 스태틱 메시 시도
+    UMeshComponent* WeaponMesh = WeaponActor->FindComponentByClass<USkeletalMeshComponent>();
+    if (!WeaponMesh)
+        WeaponMesh = WeaponActor->FindComponentByClass<UStaticMeshComponent>();
+
+    if (!WeaponMesh)
+    {
+        bUseLeftHandIK = false;
+        bUseRightHandIK = false;
+        return;
+    }
+
+    // 왼손 IK
+    if (!WeaponData->LeftHandIKSocketName.IsNone())
+    {
+        LeftHandIKLocation = WeaponMesh->GetSocketLocation(WeaponData->LeftHandIKSocketName);
+        bUseLeftHandIK = true;
+    }
+    else
+    {
+        bUseLeftHandIK = false;
+    }
+
+    // 오른손 IK
+    if (!WeaponData->RightHandIKSocketName.IsNone())
+    {
+        RightHandIKLocation = WeaponMesh->GetSocketLocation(WeaponData->RightHandIKSocketName);
+        bUseRightHandIK = true;
+    }
+    else
+    {
+        bUseRightHandIK = false;
     }
 }
