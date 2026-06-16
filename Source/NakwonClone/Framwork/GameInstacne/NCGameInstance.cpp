bb2156893bfeade06fb410676c4be9eb8831f4c9
SEOWOOJEONG <unha1@naver.com>
2026-06-16 17:21:33 +0900

    [서우정 add] Loading Screen Image 제작

--- Source/NakwonClone/Framwork/GameInstacne/NCGameInstance.cpp
+++ Source/NakwonClone/Framwork/GameInstacne/NCGameInstance.cpp
@@ -3,6 +3,8 @@
 
 #include "MoviePlayer.h"
 #include "Widgets/Images/SThrobber.h"
+#include "Fonts/SlateFontInfo.h"
+#include "Widgets/Notifications/SProgressBar.h"
 
 void UNCGameInstance::Init()
 {
@@ -17,7 +19,7 @@ FNCWeaponData* UNCGameInstance::GetWeaponData(FName WeaponID) const
 	return WeaponDataTable->FindRow<FNCWeaponData>(WeaponID, TEXT("GetWeaponData"));
 }
 
-#pragma region Loading
+#pragma region Loading Screen
 void UNCGameInstance::RegisterLoadingScreenHandlers()
 {
 	// 델리게이트 바인딩(이벤트 발생 시 함수 호출 신청)
@@ -33,12 +35,43 @@ void UNCGameInstance::RegisterLoadingScreenHandlers()
 
 void UNCGameInstance::BeginLoadingScreen(const FString& MapName)
 {
+	// 배경 텍스처가 에디터에서 연결되어 있으면 브러시로 변환하는 로직
+	if (LoadingBackgroundTexture)
+	{
+		// SlateBrush - 슬레이트(UE의 렌더링 시스템)가 이미지를 어떻게 그릴지 정의하는 구조체
+		BackgroundBrush.SetResourceObject(LoadingBackgroundTexture);
+		BackgroundBrush.ImageSize = FVector2D(1920.f, 1080.f);
+		BackgroundBrush.DrawAs = ESlateBrushDrawType::Image; // 단순 이미지로 그리기
+	}
+	
 	FLoadingScreenAttributes LoadingScreen; // 로딩 화면 설정을 담는 구조체
 	LoadingScreen.bAutoCompleteWhenLoadingCompletes = true; // 로딩 끝나면 자동으로 로딩 화면 닫힘(false 시 수동)
-	LoadingScreen.MinimumLoadingScreenDisplayTime = 2.0f; // 최소 로딩 화면 뜨는 시간
-	LoadingScreen.WidgetLoadingScreen = SNew(SThrobber); // 로딩 화면에 표시할 slate 위젯 설정(SThrobber == UE 기본 로딩 위젯)
-	// 이미지로 교체 예시 - LoadingScreen.WidgetLoadingScreen = SNew(SImage.Image(/* Slate Brush 넣기 */);
-	// 영상(.mp4, .bk2) - LoadingScreen.MoviePaths.Add(TEXT("LoadingMovie")); // Content/Movies/ 폴더에 넣은 영상 이름
+	LoadingScreen.MinimumLoadingScreenDisplayTime = 2.0f;
+	LoadingScreen.WidgetLoadingScreen = SNew(SOverlay)
+	
+	// Layer1 : Image
+		+ SOverlay::Slot().HAlign(HAlign_Fill).VAlign(VAlign_Fill)
+		[
+			SNew(SImage).Image(&BackgroundBrush)	
+		]
+	
+	// Layer2 : Text
+		+ SOverlay::Slot().HAlign(HAlign_Center).VAlign(VAlign_Bottom).Padding(0.f, 0.f, 0.f, 70.f)
+		[
+			SNew(STextBlock)
+			.Text(FText::FromString(TEXT("로딩중...")))
+			.ColorAndOpacity(FLinearColor::White)
+			.Font(FCoreStyle::GetDefaultFontStyle("Regular", 18))
+		]
+	
+	// Layer3 : Loading Bar
+		+ SOverlay::Slot().HAlign(HAlign_Center).VAlign(VAlign_Bottom).Padding(0.f, 0.f, 0.f, 50.f)
+		[
+			SNew(SBox).WidthOverride(300.f).HeightOverride(12.f)
+			[
+				SNew(SProgressBar)
+			]
+		];
 	
 	GetMoviePlayer()->SetupLoadingScreen(LoadingScreen); // 엔진의 MoviePlayer에 설정 전달 -> 로딩 화면 관리를 MoviePlayer에게 맡기는 로직
 }
@@ -48,3 +81,4 @@ void UNCGameInstance::EndLoadingScreen(UWorld* InLoadedWorld)
 	// 엔진이 자동으로 처리함(true)
 }
 #pragma endregion
+
--- Source/NakwonClone/Framwork/GameInstacne/NCGameInstance.h
+++ Source/NakwonClone/Framwork/GameInstacne/NCGameInstance.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "Player/PlayerData/NCWeaponData.h"
+#include "Styling/SlateBrush.h"
 #include "NCGameInstance.generated.h"
 
 UCLASS()
@@ -22,7 +23,7 @@ public:
 	//WeaponID로 무기 데이터 조회 (어디서든 호출 가능)
 	FNCWeaponData* GetWeaponData(FName WeaponID) const;
 	
-#pragma region Loading창 띄우기	
+#pragma region Loading Screen 띄우기	
 private:
 	void RegisterLoadingScreenHandlers(); // 로딩 화면 등록
 	void BeginLoadingScreen(const FString& MapName); // 로딩 화면 시작 콜백(레벨 전환 시 자동 호출)
@@ -30,4 +31,13 @@ private:
 	
 #pragma endregion
 	
+#pragma region Loading Screen 이미지 추가
+public:
+	// 에디터에서 배경 이미지를 연결할 프로퍼티
+	UPROPERTY(EditDefaultsOnly, Category = "Data")
+	TObjectPtr<UTexture2D> LoadingBackgroundTexture;
+	
+private:
+	FSlateBrush BackgroundBrush; // slate 브러시를 멤버변수로 보관(BegineLoadingScreen 호출 시 생성)	
+#pragma endregion
 };
