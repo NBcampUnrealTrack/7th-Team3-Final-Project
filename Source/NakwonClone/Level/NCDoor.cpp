9234ec82abc82d98e77586b039c4fa91741d92b3
YoonjaeDong <yjdong114@naver.com>
2026-06-22 19:11:17 +0900

    [동윤재 add] 혈흔 데칼 추가, 기타 에셋 콜리전 그림자 수정

---
+++ Source/NakwonClone/Level/NCDoor.cpp
@@ -0,0 +1,127 @@
+﻿#include "NCDoor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Curves/CurveFloat.h"
+#include "Kismet/GameplayStatics.h"
+
+ANCDoor::ANCDoor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	RootComponent = SceneRoot;
+
+	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
+	DoorMesh->SetupAttachment(SceneRoot);
+
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Box1"));
+	TriggerBox->SetupAttachment(SceneRoot);
+	TriggerBox->SetBoxExtent(FVector(100.f, 100.f, 100.f));
+	TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	TriggerBox->SetCollisionObjectType(ECC_WorldStatic);
+	TriggerBox->SetCollisionResponseToAllChannels(ECR_Ignore);
+	TriggerBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	TriggerBox->SetGenerateOverlapEvents(true);
+
+	DoorTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("DoorTimeline"));
+}
+
+void ANCDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &ANCDoor::OnBoxBeginOverlap);
+	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &ANCDoor::OnBoxEndOverlap);
+
+	// 타임라인 바인딩
+	if (DoorCurve)
+	{
+		FOnTimelineFloat ProgressDelegate;
+		ProgressDelegate.BindUFunction(this, FName("HandleDoorProgress"));
+		DoorTimeline->AddInterpFloat(DoorCurve, ProgressDelegate);
+
+		FOnTimelineEvent FinishedDelegate;
+		FinishedDelegate.BindUFunction(this, FName("HandleDoorFinished"));
+		DoorTimeline->SetTimelineFinishedFunc(FinishedDelegate);
+	}
+}
+
+// Door 회전
+void ANCDoor::HandleDoorProgress(float Value)
+{
+	DoorMesh->SetRelativeRotation(FRotator(0.f, Value * OpenYaw, 0.f));
+}
+
+// 애니메이션 잠금 해제
+void ANCDoor::HandleDoorFinished()
+{
+	bIsOpen = !bIsOpen;
+	bIsAnimating = false;
+}
+
+void ANCDoor::OnBoxBeginOverlap(UPrimitiveComponent*, AActor*, UPrimitiveComponent*,
+	int32, bool, const FHitResult&)
+{
+	bIsPlayerInRange = true;
+}
+
+void ANCDoor::OnBoxEndOverlap(UPrimitiveComponent*, AActor*, UPrimitiveComponent*, int32)
+{
+	bIsPlayerInRange = false;
+}
+
+// 상호작용
+void ANCDoor::Interact_Implementation(AActor* Interactor)
+{
+	// 애니메이션 중이면 무시
+	if (bIsAnimating)
+	{
+		return;
+	}
+	bIsAnimating = true;
+
+	if (bIsOpen)
+	{
+		// 닫기: 사운드 역재생
+		if (CloseSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(this, CloseSound, GetActorLocation());
+		}
+		DoorTimeline->Reverse();
+	}
+	else
+	{
+		// 열기: 사운드 → 정재생
+		if (OpenSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(this, OpenSound, GetActorLocation());
+		}
+		DoorTimeline->PlayFromStart();
+	}
+}
+
+bool ANCDoor::CanInteract_Implementation(AActor* Interactor)
+{
+	return !bIsAnimating;
+}
+
+// 외곽선(CustomDepth) on/off
+void ANCDoor::ToggleHighlight_Implementation(bool bHighlight)
+{
+	if (bHighlight)
+	{
+		DoorMesh->SetRenderCustomDepth(true);
+		DoorMesh->SetCustomDepthStencilValue(HighlightStencilValue);
+	}
+	else
+	{
+		DoorMesh->SetRenderCustomDepth(false);
+	}
+}
+
+FText ANCDoor::GetInteractPrompt_Implementation()
+{
+	return bIsOpen
+		? FText::FromString(TEXT("닫기"))
+		: FText::FromString(TEXT("열기"));
+}
---
+++ Source/NakwonClone/Level/NCDoor.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TimelineComponent.h"
+#include "NakwonClone/Common/NCInteractableInterface.h"
+#include "NCDoor.generated.h"
+
+class UBoxComponent;
+class UStaticMeshComponent;
+class UCurveFloat;
+class USoundBase;
+
+UCLASS()
+class NAKWONCLONE_API ANCDoor : public AActor, public INCInteractableInterface
+{
+	GENERATED_BODY()
+
+public:
+	ANCDoor();
+
+protected:
+	virtual void BeginPlay() override;
+
+	// 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door")
+	USceneComponent* SceneRoot;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door")
+	UStaticMeshComponent* DoorMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door")
+	UBoxComponent* TriggerBox;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door")
+	UTimelineComponent* DoorTimeline;
+
+	// 디자이너 설정
+	// 시간 = 열리는 속도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Door|Setup")
+	UCurveFloat* DoorCurve;
+
+	// 열리는 각도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Door|Setup")
+	float OpenYaw = 90.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Door|Sound")
+	USoundBase* OpenSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Door|Sound")
+	USoundBase* CloseSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Door|Highlight")
+	int32 HighlightStencilValue = 1;
+
+	// 상태
+	UPROPERTY(BlueprintReadOnly, Category = "Door|State")
+	bool bIsOpen = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Door|State")
+	bool bIsAnimating = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Door|State")
+	bool bIsPlayerInRange = false;
+
+	// 타임라인 콜백
+	UFUNCTION()
+	void HandleDoorProgress(float Value);
+
+	UFUNCTION()
+	void HandleDoorFinished();
+
+	// 오버랩
+	UFUNCTION()
+	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnBoxEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:
+	virtual void Interact_Implementation(AActor* Interactor) override;
+	virtual bool CanInteract_Implementation(AActor* Interactor) override;
+	virtual void ToggleHighlight_Implementation(bool bHighlight) override;
+	virtual FText GetInteractPrompt_Implementation() override;
+};
