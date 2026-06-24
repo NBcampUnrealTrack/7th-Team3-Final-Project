a60087a6045b9d1d66eabd980762a61d9b7f7660
dksktlq5-pixel <dksktlq5@gmail.com>
2026-06-24 19:22:08 +0900

    [박찬우] 쓸모없는거 삭제

--- Source/NakwonClone/Common/NCGameplayTags.h
+++ Source/NakwonClone/Common/NCGameplayTags.h
@@ -3,40 +3,20 @@
 #include "CoreMinimal.h"
 #include "NativeGameplayTags.h"
 
-/*
- * [새로운 태그 추가하는 방법]
- * 관련된 성격의 namespace(네임스페이스)를 찾거나 새로 만들기
- * 네임스페이스 내부에 아래 예시 매크로를 선언
- * 예시: UE_DECLARE_GAMEPLAY_TAG_EXTERN(MyNewState);
- * 소스(.cpp) 파일로 이동해서 실제 문자열을 매핑
- */
-
 // 캐릭터 상태 및 액션 태그
 namespace NCCharacter
 {
-	/* 
-	 * [다른 C++ 파일에서 사용하는 방법]
-	 * 사용하려는 파일 상단에 #include "Common/NCGameplayTags.h" 를 추가
-	 * 아래 예시 코드와 같이 네임스페이스와 변수명을 조합해 사용
-	 * 예시 코드:
-	 * if (CurrentMovementState == EMovementState::Sprint)
-	 * {
-	 * // 캐릭터에게 달리기 태그 부여
-	 * GameplayTagContainer.AddTag(NCCharacter::Sprint);
-	 * }
-	 */
-	
 	// 캐릭터 종류
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Monster);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Monster_Walker);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Monster_Runner);
-	
+
 	// 이동 관련
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Walk);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Jog);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sprint);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(CrouchSprint); //헌호수정
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(CrouchSprint);
 
 	// 상태 관련
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attacking);
@@ -44,50 +24,52 @@ namespace NCCharacter
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Landed);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Crouch);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InventoryOpen);
-	
+
 	// 생존 관련
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Alive);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Downed);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Dead);
-};
+}
 
 namespace NCItemType
 {
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Equipment); // 장비 아이템
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Consumable); // 소모품
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Material); // 재료
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Currency); // 화폐
-};
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Equipment);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Consumable);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Material);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Currency);
+}
 
 namespace NCItemTag
 {
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Weapon);  // 무기
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Armor);   // 방어구
-	
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Food);  // 음식 아이템
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Heal);    // 회복 아이템
-	
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Credit);  // 크레딧
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Quest);   // 퀘스트 아이템
-};
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Weapon);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Armor);
+
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Food);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Heal);
+
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Credit);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Quest);
+}
 
 namespace NCEquip
 {
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Hand_Left);   // 왼손 장착 가능
-};
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Hand_Left);
+}
 
 namespace NCLootBox
 {
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_BeingLooted); // 누군가 파밍 중인 상태
-};
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_BeingLooted);
+}
 
 // 무기 관련 태그
 namespace NCWeapon
 {
 	// 무기 타입
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Type_Unarmed);    // 맨손
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Type_OneHanded);  // 한손 (크로우바, 칼)
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Type_TwoHanded);  // 양손 (도끼, 배트)
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Type_Unarmed);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Type_OneHanded);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Type_TwoHanded);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Type_Pistol);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Type_Shotgun);
 
 	// 무기 무게
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Weight_Light);
@@ -95,20 +77,28 @@ namespace NCWeapon
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Weight_Heavy);
 
 	// 무기 액션
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_Attacking);   // 공격 중
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_SwingDelay);  // 공격 후 딜레이
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_Swapping);    // 무기 교체 중
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_Attacking);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_SwingDelay);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_Swapping);
+
+	// 총기 / 조준 액션
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_Aiming);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_Firing);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_Reloading);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_EmptyReloading);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_Chambering);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_PumpAction);
 
 	// 무기 상태
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Equipped);  // 장착됨
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Broken);    // 파손
-};
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Equipped);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Broken);
+}
 
-// 데미지 데이터 태그 (SetByCaller용)
+// 데미지 데이터 태그
 namespace NCData
 {
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Damage);
-};
+}
 
 namespace NCGameStateTags
 {
@@ -118,8 +108,8 @@ namespace NCGameStateTags
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameStart);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameOver);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameClear);
-	
+
 	// 싱글, 멀티 여부
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(SinglePlay);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(MultiPlay);
-};
+}
--- Source/NakwonClone/Player/PlayerAnimation/NCAnimInstance.h
+++ Source/NakwonClone/Player/PlayerAnimation/NCAnimInstance.h
@@ -63,13 +63,43 @@ public:
     bool GetHasWeapon() const { return bHasWeapon; }
 
     UFUNCTION(BlueprintPure, Category = "Weapon")
+    bool GetIsUnarmed() const { return bIsUnarmed; }
+
+    UFUNCTION(BlueprintPure, Category = "Weapon")
     bool GetIsOneHandedWeapon() const { return bIsOneHandedWeapon; }
 
     UFUNCTION(BlueprintPure, Category = "Weapon")
     bool GetIsTwoHandedWeapon() const { return bIsTwoHandedWeapon; }
 
     UFUNCTION(BlueprintPure, Category = "Weapon")
+    bool GetIsPistolWeapon() const { return bIsPistolWeapon; }
+
+    UFUNCTION(BlueprintPure, Category = "Weapon")
+    bool GetIsShotgunWeapon() const { return bIsShotgunWeapon; }
+
+    UFUNCTION(BlueprintPure, Category = "Weapon|State")
     bool GetIsAttacking() const { return bIsAttacking; }
+
+    UFUNCTION(BlueprintPure, Category = "Weapon|State")
+    bool GetIsAiming() const { return bIsAiming; }
+
+    UFUNCTION(BlueprintPure, Category = "Weapon|State")
+    bool GetIsFiring() const { return bIsFiring; }
+
+    UFUNCTION(BlueprintPure, Category = "Weapon|State")
+    bool GetIsReloading() const { return bIsReloading; }
+
+    UFUNCTION(BlueprintPure, Category = "Weapon|State")
+    bool GetIsEmptyReloading() const { return bIsEmptyReloading; }
+
+    UFUNCTION(BlueprintPure, Category = "Weapon|State")
+    bool GetIsSwappingWeapon() const { return bIsSwappingWeapon; }
+
+    UFUNCTION(BlueprintPure, Category = "Weapon|State")
+    bool GetIsChambering() const { return bIsChambering; }
+
+    UFUNCTION(BlueprintPure, Category = "Weapon|State")
+    bool GetIsPumpAction() const { return bIsPumpAction; }
 
     UFUNCTION(BlueprintPure, Category = "Aim")
     float GetAimYaw() const { return AimYaw; }
@@ -88,6 +118,27 @@ public:
 
     UFUNCTION(BlueprintCallable, Category = "Locomotion|Stop")
     void SetStopDirection(float NewStopDirection) { StopDirection = NewStopDirection; }
+
+    UFUNCTION(BlueprintCallable, Category = "Weapon|State")
+    void SetIsAiming(bool bNewValue) { bIsAiming = bNewValue; }
+
+    UFUNCTION(BlueprintCallable, Category = "Weapon|State")
+    void SetIsFiring(bool bNewValue) { bIsFiring = bNewValue; }
+
+    UFUNCTION(BlueprintCallable, Category = "Weapon|State")
+    void SetIsReloading(bool bNewValue) { bIsReloading = bNewValue; }
+
+    UFUNCTION(BlueprintCallable, Category = "Weapon|State")
+    void SetIsEmptyReloading(bool bNewValue) { bIsEmptyReloading = bNewValue; }
+
+    UFUNCTION(BlueprintCallable, Category = "Weapon|State")
+    void SetIsSwappingWeapon(bool bNewValue) { bIsSwappingWeapon = bNewValue; }
+
+    UFUNCTION(BlueprintCallable, Category = "Weapon|State")
+    void SetIsChambering(bool bNewValue) { bIsChambering = bNewValue; }
+
+    UFUNCTION(BlueprintCallable, Category = "Weapon|State")
+    void SetIsPumpAction(bool bNewValue) { bIsPumpAction = bNewValue; }
 
 protected:
     UPROPERTY(BlueprintReadOnly, Category = "References")
@@ -136,10 +187,43 @@ protected:
     bool bHasWeapon = false;
 
     UPROPERTY(BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
+    bool bIsUnarmed = true;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
     bool bIsOneHandedWeapon = false;
 
     UPROPERTY(BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
     bool bIsTwoHandedWeapon = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
+    bool bIsPistolWeapon = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
+    bool bIsShotgunWeapon = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon|State", meta = (AllowPrivateAccess = "true"))
+    bool bIsAttacking = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon|State", meta = (AllowPrivateAccess = "true"))
+    bool bIsAiming = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon|State", meta = (AllowPrivateAccess = "true"))
+    bool bIsFiring = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon|State", meta = (AllowPrivateAccess = "true"))
+    bool bIsReloading = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon|State", meta = (AllowPrivateAccess = "true"))
+    bool bIsEmptyReloading = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon|State", meta = (AllowPrivateAccess = "true"))
+    bool bIsSwappingWeapon = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon|State", meta = (AllowPrivateAccess = "true"))
+    bool bIsChambering = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Weapon|State", meta = (AllowPrivateAccess = "true"))
+    bool bIsPumpAction = false;
 
     UPROPERTY(BlueprintReadOnly, Category = "Locomotion|Stop", meta = (AllowPrivateAccess = "true"))
     bool bWantsToStop = false;
@@ -163,9 +247,7 @@ protected:
     float TwoHandIKDisableRunSpeed = 350.f;
 
     float StopIKDisableTimer = 0.f;
-
     float PreviousSpeed = 0.f;
-
     bool bPreviousWantsToStop = false;
 
     UPROPERTY(BlueprintReadOnly, Category = "IK", meta = (AllowPrivateAccess = "true"))
@@ -180,9 +262,6 @@ protected:
     UPROPERTY(BlueprintReadOnly, Category = "IK", meta = (AllowPrivateAccess = "true"))
     bool bUseRightHandIK = false;
 
-    UPROPERTY(BlueprintReadOnly, Category = "IK", meta = (AllowPrivateAccess = "true"))
-    bool bIsAttacking = false;
-
     UPROPERTY(BlueprintReadOnly, Category = "Aim", meta = (AllowPrivateAccess = "true"))
     float AimYaw = 0.f;
 
@@ -190,7 +269,10 @@ protected:
     float AimPitch = 0.f;
 
 protected:
+    void UpdateReferences();
+    void UpdateLocomotion();
     void UpdateWeaponAndBlendSpace();
+    void UpdateWeaponStateTags();
     void UpdateLeftHandIK();
     void UpdateStopState(float DeltaSeconds);
 };
