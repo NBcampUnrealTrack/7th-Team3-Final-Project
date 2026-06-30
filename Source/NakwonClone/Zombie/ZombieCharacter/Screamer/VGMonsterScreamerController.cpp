8ff60666a989f14a66c417cd0c158bcc3567c02c
IMGROOT32 <henrycho0302@gmail.com>
2026-06-30 11:54:37 +0900

    [조현준 add] screamer 뼈대 추가
    screamer 뼈대 추가

---
+++ Source/NakwonClone/Zombie/ZombieCharacter/Screamer/VGMonsterScreamerController.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VGMonsterScreamerController.h"
+
+AVGMonsterScreamerController::AVGMonsterScreamerController()
+{
+	// base 생성자에서 Perception(시각/청각)이 이미 구성됨.
+	// 스크리머는 멀리서도 플레이어를 포착하도록 시야를 더 키우고 싶다면 여기서 조정:
+	// if (SightConfig)
+	// {
+	//     SightConfig->SightRadius     = 1200.f;
+	//     SightConfig->LoseSightRadius = 1600.f;
+	//     MonsterPerceptionComponent->ConfigureSense(*SightConfig);
+	// }
+}
+
+void AVGMonsterScreamerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/NakwonClone/Zombie/ZombieCharacter/Screamer/VGMonsterScreamerController.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Zombie/AI/AIController/Base/VGMonsterAIControllerBase.h"
+#include "VGMonsterScreamerController.generated.h"
+
+UCLASS()
+class NAKWONCLONE_API AVGMonsterScreamerController : public AVGMonsterAIControllerBase
+{
+	GENERATED_BODY()
+
+public:
+	AVGMonsterScreamerController();
+
+protected:
+	virtual void BeginPlay() override;
+};
