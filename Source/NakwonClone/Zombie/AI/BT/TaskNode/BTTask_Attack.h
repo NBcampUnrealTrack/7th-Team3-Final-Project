131c19d1b16c47e3cc5d26a51ab1f7342abc4641
IMGROOT32 <henrycho0302@gmail.com>
2026-07-01 20:26:37 +0900

    [조현준 add] 좀비 공격 노티파이 설정
    좀비 공격 노티파이 설정

--- Source/NakwonClone/Zombie/AI/BT/TaskNode/BTTask_Attack.h
+++ Source/NakwonClone/Zombie/AI/BT/TaskNode/BTTask_Attack.h
@@ -19,5 +19,11 @@ public:
 
 protected:
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
+	virtual EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<class AVGMonsterCharacterBase> CachedMonster = nullptr;
+
+	UBehaviorTreeComponent* CachedOwnerComp = nullptr;
 };
