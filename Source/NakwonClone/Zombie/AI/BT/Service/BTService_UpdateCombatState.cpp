8e8d0fead1188560e2af2e8f74f6d27efa3d357a
shy3939 <kpky33@naver.com>
2026-07-01 16:27:09 +0900

    [유시환] 좀비 AI4

--- Source/NakwonClone/Zombie/AI/BT/Service/BTService_UpdateCombatState.cpp
+++ Source/NakwonClone/Zombie/AI/BT/Service/BTService_UpdateCombatState.cpp
@@ -59,6 +59,9 @@ void UBTService_UpdateCombatState::TickNode(UBehaviorTreeComponent& OwnerComp, u
 	const bool bIsCombat = (NearestDist <= 500.f);
 	BB->SetValueAsBool(AVGMonsterAIControllerBase::BIsCombatKey, bIsCombat);
 	BB->SetValueAsFloat(AVGMonsterAIControllerBase::DistanceKey, NearestDist);  // ← 항상 갱신
+	// ── 2-1) 공격 거리 판별 (가까우면 공격) ──
+	const bool bIsAttack = (NearestDist <= 150.f);
+	BB->SetValueAsBool(AVGMonsterAIControllerBase::BIsAttackKey, bIsAttack);
 
 	if (bIsCombat)
 	{
