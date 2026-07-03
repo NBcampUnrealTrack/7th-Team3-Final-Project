2af419d970766aef02f6285ed6161245a350c32b
dksktlq5-pixel <dksktlq5@gmail.com>
2026-07-03 16:32:37 +0900

    [박찬우 add, fix] 7-3-1
    픽업모션 및 파이어모션 수정

---
+++ Source/NakwonClone/Player/PlayerAnimation/NCAnimNotify_PickupAttach.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "NCAnimNotify_PickupAttach.generated.h"
+
+UCLASS()
+class NAKWONCLONE_API UNCAnimNotify_PickupAttach : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+};
