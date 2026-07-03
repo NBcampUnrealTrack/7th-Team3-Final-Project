82dcd7780f02a7ea826b3939bfad5cb2f90b5d6b
wksjdb87-design <wksjdb87@gmail.com>
2026-07-03 19:19:16 +0900

    [강민승 fix] 타이틀 화면 수정, 폰트 일부 수정

--- Source/NakwonClone/Framwork/Gamemode/NCLobbyGameMode/LobbyGameMode.cpp
+++ Source/NakwonClone/Framwork/Gamemode/NCLobbyGameMode/LobbyGameMode.cpp
@@ -5,6 +5,7 @@
 ALobbyGameMode::ALobbyGameMode()
 {
 	DefaultPawnClass = nullptr;
+	bUseSeamlessTravel = true;
 }
 
 void ALobbyGameMode::BeginPlay()
@@ -38,13 +39,13 @@ void ALobbyGameMode::CheckMapReady()
 
 void ALobbyGameMode::MoveToParkingLevel()
 {
-	if (GetWorld()->IsMapChangeReady())
-	{
-		GEngine->CommitMapChange(GetWorld());
-	}
-	else
-	{
+	// if (GetWorld()->IsMapChangeReady())
+	// {
+	// 	GEngine->CommitMapChange(GetWorld());
+	// }
+	// else
+	// {
 		GetWorld()->ServerTravel("/Game/Maps/L_Parking");
 		UE_LOG(LogTemp, Warning, TEXT("[LobbyGameMode] 지하주차장 프리로딩 실패로 서버트레블 실행"));
-	}
+	// }
 }
