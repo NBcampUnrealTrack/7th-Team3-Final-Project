0b0b2ddc6ef18f8b069e3e925d9e098153a02a9e
dksktlq5-pixel <dksktlq5@gmail.com>
2026-07-03 20:22:52 +0900

    [박찬우] 오류 수정버전

---
+++ Source/NakwonClone/Player/PlayerAnimation/NCAnimNotify_PickupStore.cpp
@@ -0,0 +1,24 @@
+﻿#include "NCAnimNotify_PickupStore.h"
+#include "GameFramework/Actor.h"
+#include "NakwonClone/Player/PlayerComponent/NCInteractionComponent.h"
+
+void UNCAnimNotify_PickupStore::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::Notify(MeshComp, Animation);
+
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	if (UNCInteractionComponent* InteractionComp = Owner->FindComponentByClass<UNCInteractionComponent>())
+	{
+		InteractionComp->StorePendingLoot();
+	}
+}
