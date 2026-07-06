b4ef94d8faa928744f58e605ad44d5ecdbe7e40a
SEOWOOJEONG <unha1@naver.com>
2026-07-06 14:18:22 +0900

    [서우정 fix] 맵 로딩 단순화(Seamless Trave)

--- Source/NakwonClone/Framwork/Gamemode/NCLobbyGameMode/LobbyGameMode.h
+++ Source/NakwonClone/Framwork/Gamemode/NCLobbyGameMode/LobbyGameMode.h
@@ -9,18 +9,11 @@ UCLASS()
 class NAKWONCLONE_API ALobbyGameMode : public AGameMode
 {
 	GENERATED_BODY()
-	
+
 protected:
 	ALobbyGameMode();
-	
-	virtual void BeginPlay() override;
-	
-	FTimerHandle NextMapReadyTimerHandle;
-	
+
 public:
-	UFUNCTION(BlueprintCallable)
-	void CheckMapReady();
-	
 	UFUNCTION(BlueprintCallable)
 	void MoveToParkingLevel();
 };
