453f2b088ed3e7d5757381e9c8bb67bfe6586e1a
tkd6966 <tkd6966@gmail.com>
2026-07-08 10:11:18 +0900

    [하상빈 Fix] 정조준 해제 시 캐릭터가 이동 방향으로 돌아버리던 버그 수정

--- Source/NakwonClone/Player/PlayerCharacter/NCPlayerCharacter.cpp
+++ Source/NakwonClone/Player/PlayerCharacter/NCPlayerCharacter.cpp
@@ -785,11 +785,6 @@ void ANCPlayerCharacter::Tick(float DeltaTime)
         }
     }
 
-    if (!bAimRotationMode)
-    {
-        return;
-    }
-
     AController* OwnerController = GetController();
     if (!OwnerController)
     {
@@ -808,7 +803,7 @@ void ANCPlayerCharacter::SetAimRotationMode(bool bEnable)
 
     if (UCharacterMovementComponent* Move = GetCharacterMovement())
     {
-        Move->bOrientRotationToMovement = !bEnable;
+        Move->bOrientRotationToMovement = false;
         Move->bUseControllerDesiredRotation = false;
     }
 
